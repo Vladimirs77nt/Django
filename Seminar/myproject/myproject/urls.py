365c01b5fb57cc9e0f96236ae2ef6c5f476ef69e
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2024-03-02 11:57:34 +0500

    семинар 5

--- Seminar/myproject/myproject/urls.py
+++ Seminar/myproject/myproject/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
     path('blogs/', include('blogs.urls')),
     path('games/', include('games.urls')),
 ]
